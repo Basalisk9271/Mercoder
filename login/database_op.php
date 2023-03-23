17f6d5371c3d70ff4d6973d3a39a5fb8699345d2
megan <ross.meg1013@gmail.com>
2023-03-23 19:01:15 +0000

    adding initial login code

---
+++ login/database_op.php
@@ -0,0 +1,8 @@
+<?php
+	$con = mysqli_connect("localhost","root","wWpmX3wgUpArsP");
+	$con -> select_db("");
+    
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
