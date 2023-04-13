fedb86a38b21ef70b2336f1c1258295595711b15
cp07620 <cpanderson76@gmail.com>
2023-04-13 15:01:01 -0400

    Fixed Login redirect

--- bootstrap_folder/PHP/index.php
+++ bootstrap_folder/PHP/index.php
@@ -241,7 +241,7 @@
 		      <div class="modal-dialog login animated">
     		      <div class="modal-content">
     		         <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">onclick="javascript:window.location='../index.php'">&times;</button>
                         <h4 class="modal-title">Login with</h4>
                     </div>
                     <div class="modal-body">
