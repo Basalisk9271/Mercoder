3414cb7b0927f8a6974572516aeb542e8273f27a
Coob <CooperNovak12@gmail.com>
2023-04-23 16:53:15 -0400

    trying not to push my screen

--- bootstrap_folder/prob_population.php
+++ bootstrap_folder/prob_population.php
@@ -8,23 +8,24 @@
     if (!$con) {
         die("Connection failed: " . mysqli_connect_error());
     }
-
+    echo "test1";
     // Define an empty array to store the results
     $problems = array();
-
+    echo "test2";
     // Select the titles and descriptions from the database
     $sql = "SELECT title, description FROM problems";
     $result = mysqli_query($con, $sql);
-
+    echo "test3";
     // Loop through the results and add them to the array
     while ($row = mysqli_fetch_assoc($result)) {
         $problems[] = array("problem" => $row["title"], "desc" => $row["description"]);
     }
+    echo "test4";
     print_r($problems);
-    
+
     session_start();
     $_SESSION["problems"] = $problems;
-
+    echo "test5";
     var_dump($problems);
 
     // Close the database connection
