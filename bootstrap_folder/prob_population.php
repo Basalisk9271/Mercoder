3210341514a90a57245e01945bd2ff0a02ccbe95
Coob <CooperNovak12@gmail.com>
2023-04-23 18:24:20 -0400

    barnch

--- bootstrap_folder/prob_population.php
+++ bootstrap_folder/prob_population.php
@@ -8,24 +8,16 @@
     if (!$con) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    echo "test1";
     // Define an empty array to store the results
     $problems = array();
-    echo "test2";
     // Select the titles and descriptions from the database
     $sql = "SELECT title, description FROM problems";
     $result = mysqli_query($con, $sql);
-    echo "test3";
     // Loop through the results and add them to the array
     while ($row = mysqli_fetch_assoc($result)) {
         $problems[] = array("problem" => $row["title"], "desc" => $row["description"]);
     }
-    echo "test4";
-
     
-    #$_SESSION["problems"] = $problems;
-    echo "test5";
-    //var_dump($problems); this is working
     // Close the database connection
     mysqli_close($con);
     echo json_encode($problems);
