c6a39fe975a578a263161cfed5ee3c431fcd36fe
Gabriel Imlay <gabe.imlay9271@gmail.com>
2023-04-24 00:46:07 +0000

    Updated nav menues and such to create a more cohesive feeling

--- bootstrap_folder/home_stu.php
+++ bootstrap_folder/home_stu.php
@@ -28,21 +28,28 @@ if(!isset($_SESSION['loggedin'])) {
     <body id="page-top">
         <!-- Navigation-->
         <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
-        <nav id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand"><a href="#page-top">Home</a></li>
-                <li class="sidebar-nav-item"><a href="./PHP/logout.php">Logout</a></li>
-                <li class="sidebar-nav-item"><a href="#about">About</a></li>
-                <li class="sidebar-nav-item"><a href="#portfolio">Problems</a></li>
-            </ul>
-        </nav>
+            <nav id="sidebar-wrapper">
+                <ul class="sidebar-nav">
+                    <li class="sidebar-brand"><a href="#page-top">Home</a></li>
+                    <?php
+                        if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == 0) {
+                            echo '<li class="sidebar-nav-item"><a href="./PHP/index.php">Login</a></li>';
+                        } else {
+                            echo '<li class="sidebar-nav-item"><a href="./PHP/logout.php">Logout</a></li>';
+                        }
+                    ?>
+                    <li class="sidebar-nav-item"><a href="#about">About</a></li>
+                    <li class="sidebar-nav-item"><a href="./PHP/prob_landing.php">Problems</a></li>
+                </ul>
+                
+            </nav>
         <!-- Header-->
         <header class="masthead d-flex align-items-center">
 	    <div class="container px-4 px-lg-5 text-center">
 		<img class="img-fluid" src="assets/img/logo-no-background.png">
                 <h1 class="mt-5" style="font-family: Rufina ">Welcome, <?php echo $_SESSION['username'];?></h1> <!-- Funnel from database -->
                 <h3 class="mb-5"><em> </em></h3>
-                <a class="btn btn-primary btn-xl" href="#problems">Problems</a>
+                <a class="btn btn-primary btn-xl" href="./PHP/prob_landing.php">Problems</a>
                 <!-- <a href='./PHP/logout.php.php?action=logout'>Logout</a> -->
             </div>
 
