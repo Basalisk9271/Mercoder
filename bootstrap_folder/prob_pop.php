ff714f8692b189473ba6c944c0b57247be6374a8
Coob <CooperNovak12@gmail.com>
2023-04-27 17:51:59 -0400

    barnch

--- bootstrap_folder/prob_pop.php
+++ bootstrap_folder/prob_pop.php
@@ -1,13 +1,13 @@
 <?php
 
-    $conn = mysqli_connect("localhost","root","wWpmX3wgUpArsP","mercoder");
+    $conn = mysqli_connect("localhost","root","Rayr3qNxsYT3iG","mercoder");
     
     // Step 2: Retrieve the information from the database
     $page_id = 1; // for example
-    $sql = "SELECT title, description, sample_input, sample_output, description_input, description_output FROM probs WHERE id = $page_id";
+    $sql = "SELECT title, description, sample_input, sample_output, description_input, description_output FROM probs WHERE title = 'An Hour Away'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    
+
     mysqli_close($conn);
 
 ?>
