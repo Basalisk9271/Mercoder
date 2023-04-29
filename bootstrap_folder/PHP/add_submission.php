971c1b20537d95efdbd2ab80631d487f565a4107
Coob <CooperNovak12@gmail.com>
2023-04-28 20:40:38 -0400

    testing

--- bootstrap_folder/PHP/add_submission.php
+++ bootstrap_folder/PHP/add_submission.php
@@ -8,11 +8,13 @@
             //setting link to a variable
             $link = $_POST['link'];
             //Find a way to get the session username 
-            $user_name = $_SESSION['username'];
+            //$user_name = $_SESSION['username'];
+            $user_name = 'Bob';
             //$placeholder_username = 'TEST';
             $date = date('d-m-Y');
             $query = "INSERT into problem_attempts (user_name, submission_date, submission_link) 
                       VALUES ('$user_name','$date','$link')";
+
             //Run query
             $run = mysqli_query($con,$query);
                 if($run){
