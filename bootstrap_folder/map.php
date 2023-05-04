5df4cc530d8a2ef9b318e4581f8a20bb0e7d2393
Coob <CooperNovak12@gmail.com>
2023-05-03 23:06:57 -0400

    barnch

--- bootstrap_folder/map.php
+++ bootstrap_folder/map.php
@@ -8,6 +8,11 @@ if(!isset($_SESSION['loggedin'])) {
 } 
 require_once 'PHP/get_prob_titles.php';
 $probArr = getProbTitles();
+
+require_once('fill_locations.php');
+//Geocode API Key for function params
+$api_key = 'AIzaSyAV2jXEkwfKvpehW3TGhQMu8FXQrZ16sNQ';
+$mapmarkers = getSubmissions(5, $api_key);
 ?>
 
 <!DOCTYPE html>
@@ -212,31 +217,60 @@ $probArr = getProbTitles();
                                <h2 class="text-white mb-4"></h2>
                                <div id="googleMap" style="width:100%;height:650px;"></div>
                                <script>
-                               function myMap() {
-                               var mapProp= {
-                                 center:new google.maps.LatLng(33.264080,-82.763100),
-                                 zoom:3.75,
-                               };
-                               var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                              
+                    function myMap() {
+                        
+                        var mapProp= {
+                          center:new google.maps.LatLng(32.840694,-83.632401),
+                          zoom:5,
+                        };
+                        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-                               //make const
+                        var i, myLatLng, message, marker;
+                        if (locations.length !== 0) {
 
-                               const lat = [32.832102];
-                               const lang = [-83.648181];
-                               const info = ["Mercer University"]
+                          for (var i = 0; i < locations.length; i++) {
+                          console.log(locations[i].location);
+                          console.log(locations[i].lat);
+                          console.log(locations[i].lng);
+                          }
 
-                               for (let i = 0; i < lat.length; i++)
-                                 {
-                                   var marker = new google.maps.Marker({position: new google.maps.LatLng(lat[i],lang[i])});
-                                   marker.setMap(map);
-                                   var infowindow = new google.maps.InfoWindow({content:info[i]});
-                                   infowindow.open(map,marker);
-                                 }
-                               }
-                               </script>
-                               <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfhDoPuP4Hkf_nis_oKqwol7Tk5TuzJA8&callback=myMap"></script>
+                            // Function that runs the amount of times that there are items in the array
+                            // This allows each marker to get it's own listeniers as well as be plotted on the map.  
+                            locations.forEach(function(location, index) {
+                                myLatLng = {lat: location.lat, lng:location.lng}
+                                marker = new google.maps.Marker({
+                                    position: myLatLng, 
+                                    map: map, 
+                                    title:"Location " + (index + 1)
+                                });
+
+                               
+                                var infoWindow = new google.maps.InfoWindow({
+                                    content:location.location
+                                });
+                                
+                                var markerListener = function() {
+                                    infoWindow.open(map, this);
+                                    var pos = map.getZoom();
+                                    map.setZoom(12);
+                                    map.setCenter(this.getPosition());
+                                    window.setTimeout(function() {
+                                        map.setZoom(pos);
+                                    },5000);
+                                };
+                                
+
+                                marker.addListener('click', markerListener);
+                                
+                                marker.setMap(map);
+                            });
+                        }
+
+                        }
+                               </script>  
             </div>
-            
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfhDoPuP4Hkf_nis_oKqwol7Tk5TuzJA8&callback=myMap"></script>
         </section>
         
         <!-- Footer-->
