ecc44d5f9dc2d845e4cf9605092e2abdfed832ab
Coob <CooperNovak12@gmail.com>
2023-05-04 13:50:38 -0400

    ohdsf

--- bootstrap_folder/PHP/get_prob_id.php
+++ bootstrap_folder/PHP/get_prob_id.php
@@ -1,32 +1,17 @@
 <?php
 $probName = $_POST['probName'];
-require 'error_handling.php';
-require_once'database_op.php';
-
-echo 'Test';
-echo $probName;
-echo 'Test2';
+require('database_op.php');
+require('error_handling.php');
 
 // Retrieve the information from the database
-$sql = "SELECT id FROM probs WHERE title = '" . $probName . "'";
-$result = mysqli_query($con, $sql);
-echo $result;
-echo 'Test4';
-if (!$result) {
-    printf("Error: %s\n", mysqli_error($con));
-    exit();
-}
-
+$sql = "SELECT probid FROM probs WHERE title = '" . $probName . "'";
+$result = mysqli_query($con, mysqli_real_escape_string($con, $sql));
 $rows = array();
 while ($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row['id'];
+    $rows[] = $row['probid'];
 }
 
 mysqli_close($con);
-echo $rows[0];
+
+echo $rows[0]; // Access the first element of the $rows array
 ?>
-
-
-
-
-
