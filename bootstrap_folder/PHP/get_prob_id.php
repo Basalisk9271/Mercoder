bceca78ef647f5736f72a46ccefcac726485b15b
Coob <CooperNovak12@gmail.com>
2023-05-04 13:03:03 -0400

    barnch

--- bootstrap_folder/PHP/get_prob_id.php
+++ bootstrap_folder/PHP/get_prob_id.php
@@ -2,13 +2,13 @@
     $probName = $_POST['probName'];
     require('database_op.php');
     // Retrieve the information from the database
-    $sql = "SELECT probid FROM probs WHERE title = '" . $probName . "'";
+    $sql = "SELECT id FROM probs WHERE title = '" . $probName . "'";
     $result = mysqli_query($con, mysqli_real_escape_string($con, $sql));
     $row = mysqli_fetch_assoc($result);
 
-    $probId = (int) $row['probid'];
+    $probId = (int) $row['id'];
 
     mysqli_close($con);
-    
+
     echo $probId;
 ?>
