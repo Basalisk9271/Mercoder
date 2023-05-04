7115d158dd1cc44cb95fd4c4ce5622f5b5de4820
Coob <CooperNovak12@gmail.com>
2023-05-04 10:44:58 -0400

    barnch

--- bootstrap_folder/PHP/get_prob_titles.php
+++ bootstrap_folder/PHP/get_prob_titles.php
@@ -1,6 +1,6 @@
 <?php
 function getProbTitles() {
-    require_once('database_op.php');
+    require('database_op.php');
     // Retrieve the information from the database
     $sql = "SELECT title FROM probs";
     $result = mysqli_query($con, mysqli_real_escape_string($con, $sql));
