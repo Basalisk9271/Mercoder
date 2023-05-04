380ab9f43586fecb2b744f5cf8b1b1e3c08f2db5
Coob <CooperNovak12@gmail.com>
2023-05-03 21:27:53 -0400

    f

--- bootstrap_folder/PHP/database_op.php
+++ bootstrap_folder/PHP/database_op.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect("localhost","root","xsMDpCNzUu5Bat","mercoder");
+	$con = mysqli_connect("localhost","root","Rayr3qNxsYT3iG","mercoder");
     
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
