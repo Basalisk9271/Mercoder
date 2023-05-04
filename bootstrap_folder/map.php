b5ab6205c9d691580a5e1a6f14ff1274e4cbef7a
Coob <CooperNovak12@gmail.com>
2023-05-04 10:40:41 -0400

    barnch

--- bootstrap_folder/map.php
+++ bootstrap_folder/map.php
@@ -183,7 +183,6 @@ $mapmarkers = getSubmissions(12, $api_key);
    ini_set("display_errors", "1");
    ini_set("display_startup_errors", 1);
    error_reporting(E_ALL);
-?>
 require_once 'get_prob_titles.php';
 $probArr =  getProbTitles();
 echo $probArr;
